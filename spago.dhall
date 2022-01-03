96fcfb300d092dfcc5ffb660c116492257673143
Michael Bock <me@thought2.de>
2022-01-03 13:40:21 +0100

    Init

---
+++ spago.dhall
@@ -0,0 +1,8 @@
+{ name = "purescript-foreign-object"
+, dependencies = [ "prelude" ]
+, backend = "purenix"
+, packages = ./packages.dhall
+, sources =
+    [ "src/**/*.purs"
+    ]
+}
---
+++ test/Test/Test/Foreign/Object.purs
@@ -0,0 +1,9 @@
+module Test.Foreign.Object where
+
+import Prelude
+import Test.Miraculix (TestTree, testCase, testGroup, (@?=))
+
+tests :: TestTree
+tests =
+  testGroup "Foreign.Object"
+    []
